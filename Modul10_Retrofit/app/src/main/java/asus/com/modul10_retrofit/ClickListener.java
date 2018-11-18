213f01eb0b2e68884d3c8f2b089a6bbe9b163414
LinxAldy <linx.aldy18@gmail.com>
2018-11-19 01:14:17 +0700

    New Commit

--- Modul10_Retrofit/app/src/main/java/asus/com/modul10_retrofit/ClickListener.java
+++
@@ -1,8 +0,0 @@
-package asus.com.modul10_retrofit;
-
-import android.view.View;
-
-public interface ClickListener {
-    void onClick(View view, int position);
-    void onLongClick(View view, int position);
-}
